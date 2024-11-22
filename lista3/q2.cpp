8b78369c7d7f76b4a4714ac310eb10d886e76da7
El-Tomaz <>
2024-11-22 17:55:49 -0300

    add lista 3

---
+++ lista3/q2.cpp
@@ -0,0 +1,35 @@
+/*
+
+Crie uma função chamada trocaValores que receba dois ponteiros para variáveis inteiras 
+e troque os valores entre elas.
+
+*/
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+void trocaValores(int *valor1, int *valor2){
+	int temp = *valor2;
+	*valor2 = *valor1;
+	*valor1 = temp;
+}
+
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    trocaValores(&x, &y);
+    assert(x == 10 && y == 5);  // Valores trocados
+
+    int a = -3;
+    int b = 3;
+
+    trocaValores(&a, &b);
+    assert(a == 3 && b == -3);  // Valores trocados
+
+    cout << "Todos os testes passaram!" << endl;
+    return 0;
+}
